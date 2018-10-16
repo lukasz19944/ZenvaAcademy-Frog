b8c103d771576568207a90c805b7491a1a63cd62
lukasz19944 <lukasz19944@gmail.com>
2018-10-16 12:35:47 +0200

    Pierwsza wersja gry po ukończeniu kursu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public PlayerController player;
+    public Text levelText;
+    public Text scoreText;
+    public Text gameOverText;
+
+    public float difficultyIncrease = 1.2f;
+
+    private float highestPosition;
+    private int score = 0;
+    private int level = 1;
+    private float restartTimer = 3;
+
+	// Use this for initialization
+	void Start () {
+        gameOverText.gameObject.SetActive(false);
+
+        player.OnPlayerMoved += OnPlayerMoved;
+        player.OnPlayerEscaped += OnPlayerEscaped;
+
+        highestPosition = player.transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player == null) {
+            gameOverText.gameObject.SetActive(true);
+
+            restartTimer -= Time.deltaTime;
+            if (restartTimer <= 0f) {
+                SceneManager.LoadScene("Game");
+            }
+        }
+	}
+
+    void OnPlayerMoved() {
+        if (player.transform.position.y > highestPosition) {
+            highestPosition = player.transform.position.y;
+
+            score++;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    void OnPlayerEscaped() {
+        highestPosition = player.transform.position.y;
+
+        level++;
+        levelText.text = "Level: " + level;
+
+        foreach (EnemyController enemy in GetComponentsInChildren<EnemyController>()) {
+            enemy.speed *= difficultyIncrease;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public delegate void PlayerHandler();
+    public event PlayerHandler OnPlayerMoved;
+    public event PlayerHandler OnPlayerEscaped;
+
+    public float jumpDistance = 0.32f;
+
+    private bool jumped = false;
+    private Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // movement
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        if (!jumped) {
+            Vector2 targetPosition = Vector2.zero;
+            bool tryingToMove = false;
+
+            if (horizontalMovement != 0) {
+                tryingToMove = true;
+                targetPosition = new Vector2(
+                    transform.position.x + (horizontalMovement > 0 ? jumpDistance : -jumpDistance), 
+                    transform.position.y
+                );
+            }
+
+            if (verticalMovement != 0) {
+                tryingToMove = true;
+                targetPosition = new Vector2(
+                   transform.position.x,
+                   transform.position.y + (verticalMovement > 0 ? jumpDistance : -jumpDistance)
+               );
+            }
+
+            // check if player can move
+            Collider2D hitCollider = Physics2D.OverlapCircle(targetPosition, 0.1f);
+
+            if (tryingToMove == true && (hitCollider == null || hitCollider.GetComponent<EnemyController>() != null)) {
+                transform.position = targetPosition;
+
+                jumped = true;
+
+                GetComponent<AudioSource>().Play();
+
+                if (OnPlayerMoved != null) {        // check if anybody wants to know if the player has moved
+                    OnPlayerMoved();
+                }
+            }
+        } else {
+            if (horizontalMovement == 0 && verticalMovement == 0) {
+                jumped = false;
+            }
+        }
+
+        // keep the frog inside bounds
+        if (transform.position.y < -(Screen.height / 100f) / 2f) {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y + jumpDistance
+            );
+        }
+
+        if (transform.position.y > (Screen.height / 100f) / 2f) {
+            transform.position = startingPosition;
+
+            if (OnPlayerEscaped != null) {
+                OnPlayerEscaped();
+            }
+        }
+
+        if (transform.position.x < -(Screen.width / 100f) / 2f) {
+            transform.position = new Vector3(
+                transform.position.x + jumpDistance,
+                transform.position.y
+            );
+        }
+
+        if (transform.position.x > (Screen.width / 100f) / 2f) {
+            transform.position = new Vector3(
+                transform.position.x - jumpDistance,
+                transform.position.y
+            );
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.GetComponent<EnemyController>() != null) {
+            Destroy(gameObject);
+        }
+    }
+}
